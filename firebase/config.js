8c1961f5aeb9cd8ab2c9897cca112b983c04cc49
Tautvydas <95254346+zarta21@users.noreply.github.com>
2023-01-07 14:38:25 +0100

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,15 +2,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 import { serverTimestamp } from "firebase/firestore";
 
- // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
 
@@ -18,8 +10,6 @@ const firebaseConfig = {
 
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const projectStorage = getStorage(app);
 const projectFirestore = getFirestore();
