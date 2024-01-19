4113a6330c3c00db4b95e338a33ddbcefd6c421c
Tautvydas <95254346+zarta21@users.noreply.github.com>
2024-01-19 14:34:18 +0100

    Add files via upload

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
+
+const UploadForm = () => {
+    const [files, setFiles] = useState([]);
+    const [error, setError] = useState(null)
+    const types = ["image/jpeg"];
+
+    const selectedImg = (e) => {
+
+        for (let i = 0; i < e.target.files.length; i++) {
+            let selected = e.target.files[i];
+            // selected["id"] = Math.random();
+            if (selected && types.includes(selected.type)) {
+                setFiles([...e.target.files]);
+                setError("");
+            } else {
+                setFiles([]);
+                setError("Please select .jpeg image file");
+                // alert("Please select .jpeg image file")
+            }
+        }
+    }
+    return (
+        <form className="upload">
+            <label>
+                <input type="file" multiple onChange={selectedImg} />
+                <FontAwesomeIcon icon={faPlusCircle} />
+            </label>
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {files && <ProgressBar files={files} />} 
+            </div>
+        </form>
+    );
+}
+
+export default UploadForm;
